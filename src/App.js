9b98603a3bd45a3f52bb0f6830b7938d6ba8a5a6
Abhishek Bharti <abhishek@Abhisheks-MacBook-Air.local>
2023-09-28 00:01:29 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
 import "./App.css";
 import { Footer, Header } from "./components";
+import { Home, Quiz, Result } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header/>
+        <Routes>
+           <Route exact path="/"  Component={Home}/> 
+           <Route exact path="quiz" Component={Quiz}/>
+           <Route exact path="result" Component={Result}/>
+        </Routes>
       </div>
       <Footer/>
     </BrowserRouter>
