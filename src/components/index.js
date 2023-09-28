6e8ac4f4e3b306449ee05667ed6e8c9e1d47fea2
Abhishek Bharti <abhishek@Abhisheks-MacBook-Air.local>
2023-09-28 14:01:47 +0530

    quiz page completed

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Header} from './Header/Header'
 export {default as Footer} from './Footer/Footer'
 export {default as ErrorMessage} from './ErrorMessage/ErrorMessage'
+export {default as Question} from './Question/Question'
